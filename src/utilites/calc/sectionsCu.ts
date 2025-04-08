98f2b1193ad26a20f6ba4d80f5d761a77f3123fd
Volodymyr Nerovnia <nerv@i.ua>
2025-04-08 12:38:18 +0300

    Created sectionsCu module - data sections for wires

---
+++ src/utilites/calc/sectionsCu.ts
@@ -0,0 +1,102 @@
+export const sectionsCu = [
+    {
+      current: 11,
+      section: 0.5,
+    },
+    {
+      current: 15,
+      section: 0.75,
+    },
+    {
+      current: 17,
+      section: 1,
+    },
+    {
+      current: 20,
+      section: 1.2,
+    },
+    {
+      current: 23,
+      section: 1.5,
+    },
+    {
+      current: 26,
+      section: 2,
+    },
+    {
+      current: 30,
+      section: 2.5,
+    },
+    {
+      current: 34,
+      section: 3,
+    },
+    {
+      current: 41,
+      section: 4,
+    },
+    {
+        current: 46,
+        section: 5,
+      },
+      {
+        current: 50,
+        section: 6,
+      },
+      {
+        current: 62,
+        section: 8,
+      },
+      {
+        current: 80,
+        section: 10,
+      },
+      {
+        current: 100,
+        section: 16,
+      },
+      {
+        current: 140,
+        section: 25,
+      },
+      {
+        current: 170,
+        section: 35,
+      },
+      {
+        current: 215,
+        section: 50,
+      },
+      {
+        current: 270,
+        section: 70,
+      },
+      {
+        current: 330,
+        section: 95,
+      },
+      {
+        current: 385,
+        section: 120,
+      },
+      {
+        current: 440,
+        section: 150,
+      },
+      {
+        current: 510,
+        section: 185,
+      },
+      {
+        current: 605,
+        section: 240,
+      },
+      {
+        current: 695,
+        section: 300,
+      },
+      {
+        current: 830,
+        section: 400,
+      },
+  ];
